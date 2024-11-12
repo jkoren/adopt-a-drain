c72309a5b164e2ebb954bb75d7384c71dfd675a7
jkoren <j.korenstein@gmail.com>
2024-11-11 19:40:26 -0500

    added dropdowns to promote user and demote admin

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  def admin_emails
+      users = User.all.select { |u| u.admin} # only admins
+      user_emails = users.map{|user| user.email}
+      return user_emails
+  end
+
+  def non_admin_emails
+      users = User.all.select { |u| !u.admin} # only non-admins
+    user_emails = users.map{|user| user.email}
+    return user_emails
+  end
+
 end
